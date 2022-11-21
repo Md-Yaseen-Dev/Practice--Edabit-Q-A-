40d43a2a63775efc743dbfd0f5a91ff5a568b7ca
mohammedyaseen2211 <ysn6040@gmail.com>
2022-11-21 16:38:58 +0530

    arrays challangers  are added to very easy

---
+++ Arrays/Very Easy/2.Pair management.js
@@ -0,0 +1,20 @@
+// Given two arguments, return an array which contains these two arguments.
+
+// Examples
+// makePair(1, 2) ➞ [1, 2]
+
+// makePair(51, 21) ➞ [51, 21]
+
+// makePair(512124, 215) ➞ [512124, 215]
+// Notes
+// N/A
+
+
+function makepair(num1 , num2){
+
+    return [num1 , num2];
+}
+
+console.log(makepair(1,2));
+console.log(makepair(51,21));
+console.log(makepair(512124,215));
