3ecac9ec0346380f2dccde266bd559c73c2a732a
mohammedyaseen2211 <ysn6040@gmail.com>
2022-11-29 17:44:35 +0530

    challenge completed

--- Arrays/Very Easy/36. Destructuring Arrays II .js
+++ Arrays/Very Easy/36. Destructuring Arrays II .js
@@ -19,8 +19,8 @@
 
 const arr = ["cars", "planes", ["trains", ["motorcycles"]]];
 
-let [trans1 , trans2 , [trans3, [trans4]]] = arr ;  // check once.
-
+let [trans1 , trans2 , [trans3, [trans4]]] = arr ;  // check once
+                                                      
 console.log(trans1);
 console.log(trans2);
 console.log(trans3);
