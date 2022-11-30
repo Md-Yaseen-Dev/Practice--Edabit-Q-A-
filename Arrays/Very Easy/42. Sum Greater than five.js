04ff463c7b62b6f95571637519d4513ad505cc75
mohammedyaseen2211 <ysn6040@gmail.com>
2022-11-30 16:25:58 +0530

    challenge completed

---
+++ Arrays/Very Easy/42. Sum Greater than five.js
@@ -0,0 +1,20 @@
+// Sum Greater Than Five
+// Write a function that returns the sum of elements greater than 5, in the given array .
+
+// Examples
+// sumFive([1, 5, 20, 30, 4, 9, 18]) ➞ 77
+
+// sumFive([1, 2, 3, 4]) ➞ 0
+
+// sumFive([10, 12, 28, 47, 55, 100]) ➞ 252
+// Notes
+// Find all the elements greater than 5, not the elements greater than or equal to 5.
+
+function sumFive(arr) {
+   const filter = arr.filter(element => element > 5);  // Is this correct way.
+
+   return filter.reduce((a,b)=> a + b , 0);
+};
+console.log(sumFive([1, 5, 20, 30, 4, 9, 18]));
+console.log(sumFive([1,2,3,4]));
+console.log(sumFive([10, 12, 28, 47, 55, 100]));
