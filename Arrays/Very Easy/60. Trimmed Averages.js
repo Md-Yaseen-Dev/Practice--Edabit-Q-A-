7feee67ff31b34775a331915832df4e6e50b9be3
mohammedyaseen2211 <ysn6040@gmail.com>
2022-12-17 23:31:18 +0530

    added a file

---
+++ Arrays/Very Easy/60. Trimmed Averages.js
@@ -0,0 +1,43 @@
+// Trimmed Averages
+// Given an array of numbers, remove the largest and smallest numbers, and calculate the average of the remaining numbers.
+
+// Examples
+// trimmedAverages([4, 5, 7, 100]) ➞ 6
+// // Average of 5 and 7
+
+// trimmedAverages([10, 25, 5, 15, 20]) ➞ 15
+// // Average of 10, 15 and 20
+
+// trimmedAverages([1, 1, 1]) ➞ 1
+// // 1
+// Notes
+// Round to the nearest whole number.
+// Array size is greater than 2.
+
+function trimmedAverages(arr) {
+    let biggestNum = 0;
+    let smallestNum = arr[0];
+    for (i = 0; i < arr.length; i++) {
+
+        if (biggestNum < arr[i]) {
+
+            biggestNum = arr[i]
+        }
+
+        if (smallestNum > arr[i]) {
+            smallestNum = arr[i]
+        }
+    }
+    const newarr = arr.filter(elem => (elem != biggestNum) && (elem != smallestNum));
+
+    // for (j = 0; j < newarr.length; j++) {
+
+    //     newarr[j] = newarr[j] / newarr.length
+    // }
+    return newarr.reduce((a, b) => (a + b));
+}
+
+console.log(trimmedAverages([4, 5, 7, 100]));
+console.log(trimmedAverages([10, 25, 5, 15, 20]));
+// console.log(trimmedAverages([1, 1, 1]));
+
