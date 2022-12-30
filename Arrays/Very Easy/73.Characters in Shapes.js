732715ae768f539798f351439cf33a91cf6145b0
mohammedyaseen2211 <ysn6040@gmail.com>
2022-12-31 00:01:55 +0530

    challenge completed

---
+++ Arrays/Very Easy/73.Characters in Shapes.js
@@ -0,0 +1,35 @@
+// Characters in Shapes
+// Create a function to calculate how many characters in total are needed to make up the shape. You will be given an array of strings which make up a shape in the compiler (i.e. a square, a rectangle or a line).
+
+// Examples
+// countCharacters([
+//   "###",
+//   "###",
+//   "###"
+// ]) ➞ 9
+
+// countCharacters([
+//   "22222222",
+//   "22222222",
+// ]) ➞ 16
+
+// countCharacters([
+//   "------------------"
+// ]) ➞ 18
+
+// countCharacters([]) ➞ 0
+
+// countCharacters(["", ""]) ➞ 0
+// Notes
+// Return 0 if the given array is empty.
+
+function countCharacters(arr) {
+
+    return (arr == 0) ? 0 : arr.reduce((a, b) => a + b).length;
+}
+
+console.log(countCharacters(["###", "###", "###"]));
+console.log(countCharacters(["22222222", "22222222"]));
+console.log(countCharacters(["------------------"]));
+console.log(countCharacters([]));
+console.log(countCharacters(["", ""]));
