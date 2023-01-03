d83d1974085241b633f901a33480a8d6492f1b6e
mohammedyaseen2211 <ysn6040@gmail.com>
2023-01-03 22:48:00 +0530

    challenge completed

---
+++ Arrays/Very Easy/78. Unlucky 13.js
@@ -0,0 +1,22 @@
+// Unlucky 13
+// Given a sorted array of numbers, remove any numbers that are divisible by 13. Return the amended array.
+
+// Examples
+// unlucky13([53, 182, 435, 591, 637]) ➞ [53, 435, 591]
+// // 182 and 637 are divisible by 13.
+
+// unlucky13([24, 316, 393, 458, 1279]) ➞ [24, 316, 393, 458, 1279]
+// // No numbers in the array are divisible by 13.
+
+// unlucky13([104, 351, 455, 806, 871]) ➞ []
+// // All numbers in the array are divisible by 13.
+// Notes
+// N/A
+
+function unlucky13(arr) {
+
+    return arr.filter(elem => elem % 13 !== 0);
+}
+console.log(unlucky13([53, 182, 435, 591, 637]));
+console.log(unlucky13([24, 316, 393, 458, 1279]));
+console.log(unlucky13([104, 351, 455, 806, 871]));cd 
