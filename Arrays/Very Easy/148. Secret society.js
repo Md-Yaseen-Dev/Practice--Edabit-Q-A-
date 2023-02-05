45f5cfe5e9789a8542a30b7ab1eb83e2a71cda5f
mohammedyaseen2211 <ysn6040@gmail.com>
2023-02-06 00:01:10 +0530

    challenge completed

---
+++ Arrays/Very Easy/148. Secret society.js
@@ -0,0 +1,24 @@
+// Secret Society
+// A group of friends have decided to start a secret society. The name will be the first letter of each of their names, sorted in alphabetical order.
+
+// Create a function that takes in an array of names and returns the name of the secret society.
+
+// Examples
+// societyName(["Adam", "Sarah", "Malcolm"]) ➞ "AMS"
+
+// societyName(["Harry", "Newt", "Luna", "Cho"]) ➞ "CHLN"
+
+// societyName(["Phoebe", "Chandler", "Rachel", "Ross", "Monica", "Joey"]) ➞ "CJMPRR"
+// Notes
+// The secret society's name should be entirely uppercased.
+
+
+function societyName(arr) {
+
+    return arr.map(elem => [...elem][0]).sort().join("")
+}
+console.log(societyName(["Adam", "Sarah", "Malcolm"]));
+
+console.log(societyName(["Harry", "Newt", "Luna", "Cho"]));
+
+console.log(societyName(["Phoebe", "Chandler", "Rachel", "Ross", "Monica", "Joey"]));
