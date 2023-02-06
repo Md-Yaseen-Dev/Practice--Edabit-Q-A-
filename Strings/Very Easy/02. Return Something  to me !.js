bae1500a97a1c9d7bb1f18b6d09326d5c28205c4
mohammedyaseen2211 <ysn6040@gmail.com>
2023-02-07 00:30:27 +0530

    challenge completed

---
+++ Strings/Very Easy/02. Return Something  to me !.js
@@ -0,0 +1,23 @@
+// Return Something to Me!
+// Write a function that returns the string "something" joined with a space " " and the given argument a.
+
+// Examples
+// giveMeSomething("is better than nothing") ➞ "something is better than nothing"
+
+// giveMeSomething("Bob Jane") ➞ "something Bob Jane"
+
+// giveMeSomething("something") ➞ "something something"
+// Notes
+// Assume an input is given.
+
+
+function giveMeSomething(a) {
+
+    // return "something" + " " + a ;
+
+    return `something ${a}`
+}
+
+console.log(giveMeSomething("is better than nothing"));
+console.log(giveMeSomething("is better than nothing"));
+console.log(giveMeSomething("something"));
