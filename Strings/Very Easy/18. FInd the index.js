52717f55c5c33107b36b0ea55579ccb914d02509
mohammedyaseen2211 <ysn6040@gmail.com>
2023-02-15 16:33:33 +0530

    challange completed

---
+++ Strings/Very Easy/18. FInd the index.js
@@ -0,0 +1,23 @@
+// Find the Index
+// Create a function that takes an array and a string as arguments and returns the index of the string.
+
+// Examples
+// findIndex(["hi", "edabit", "fgh", "abc"], "fgh") ➞ 2
+
+// findIndex(["Red", "blue", "Blue", "Green"], "blue") ➞ 1
+
+// findIndex(["a", "g", "y", "d"], "d") ➞ 3
+
+// findIndex(["Pineapple", "Orange", "Grape", "Apple"], "Pineapple") ➞ 0
+// Notes
+// Don't forget to return the result.
+// If you are stuck, find help in the Resources tab.
+
+function findIndex(arr, val) {
+
+    return arr.indexOf(val);
+}
+console.log(findIndex(["hi", "edabit", "fgh", "abc"], "fgh"));
+console.log(findIndex(["Red", "blue", "Blue", "Green"], "blue"));
+console.log(findIndex(["a", "g", "y", "d"], "d"));
+console.log(findIndex(["Pineapple", "Orange", "Grape", "Apple"], "Pineapple"));
