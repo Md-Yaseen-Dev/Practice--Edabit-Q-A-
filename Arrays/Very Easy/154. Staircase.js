3000055e8503e7cd9288a4fb2072d9d3e28f718c
mohammedyaseen2211 <ysn6040@gmail.com>
2023-02-22 02:13:21 +0530

    challenge completed"

---
+++ Arrays/Very Easy/154. Staircase.js
@@ -0,0 +1,31 @@
+// function staircase(n) {
+//     // Write your code here
+//     let str = " ";
+//     for (let i = 0; i <= n; i++) {
+
+//         str += " ".repeat(n - i)
+
+//         for (let j = 0; j < i; j++) {
+
+//             str += "#"
+
+//         }
+//         str += "\n"
+//     }
+//     return str
+
+// }
+// console.log(staircase(4))
+
+
+// --- we can write like these to
+
+function staircase(n) {
+    let str = ""
+    for (let i = 1; i <= n; i++) {
+
+        str += " ".repeat(n - i) + "#".repeat(i) + "\n"
+    }
+    return str
+}
+console.log(staircase(6))
